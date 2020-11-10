095066224aa88197e3e60d6f4246c70c5101229f
Amy Lofgren <amylofgren@amys-mbp.myfiosgateway.com>
2020-11-10 08:10:49 -0500

    adding react code

---
+++ testinggithubpages/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <Button color="primary">A Material Button</Button>
+    </div>
+  );
+}
+
+export default App;
